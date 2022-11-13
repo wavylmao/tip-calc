c43dd53eca2b1b112289b95a4da19a61fffaf0ea
ddev187 <yawnhenley@gmail.com>
2022-11-13 15:13:22 -0500

    Finish frontend layout for pc

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Results = () => {
-  return <div>Results</div>;
+  return (
+    <div className="rounded-md ml-10 w-80 h-[330px] bg-very-dark-cyan font-space-mono p-10 relative">
+      <h3 className="text-white">Tip Amount</h3>
+      <p className="text-grayish-cyan mb-[32px]">/person</p>
+      <h2 className="text-4xl text-strong-cyan absolute right-10 top-10">
+        $0.00
+      </h2>
+      <h3 className="text-white">Total</h3>
+      <p className="text-grayish-cyan">/person</p>
+      <h2 className="text-4xl text-strong-cyan absolute right-10 top-28">
+        $0.00
+      </h2>
+      <button className="absolute bottom-8 left-[10%] w-64 h-10 bg-strong-cyan rounded-md text-very-dark-cyan">
+        RESET
+      </button>
+    </div>
+  );
 };
 
 export default Results;
